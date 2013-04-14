dd29e75c4b0e5de827aed02b3d7909b755a438bb
miguel.martins.gama@gmail.com <miguel.martins.gama@gmail.com@3b17d88b-3fc6-c5de-6d68-e005c3272273>
2013-04-14 15:39:07 +0000

    Added missing copyright/license header to the source code files of the plug-in.

--- PROMNeT++ Plugin/src/com/googlecode/promnetpp/eclipseplugin/Activator.java
+++ PROMNeT++ Plugin/src/com/googlecode/promnetpp/eclipseplugin/Activator.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2013, Miguel Martins
+ * Use is subject to license terms.
+ *
+ * This source code file is provided under the MIT License. Full licensing
+ * terms should be available in the form of text files. The standard source code
+ * distribution provides a LICENSE.txt file which can be consulted for licensing
+ * details.
+ */
 package com.googlecode.promnetpp.eclipseplugin;
 
 import org.eclipse.jface.resource.ImageDescriptor;
